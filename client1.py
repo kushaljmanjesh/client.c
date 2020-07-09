43b7054376465d2460ba6612a88ad243d5b9ffe4
kushaljmanjesh <67941743+kushaljmanjesh@users.noreply.github.com>
2020-07-09 18:33:53 +0530

    client code

--- client1.py
+++ client1.py
@@ -1,25 +1,12 @@
+
 import socket
 
-def client_program():
-    host = socket.gethostname()  # as both code is running on same pc
-    port = 27015  # socket port number
+HOST = '127.0.0.1'  # The server's hostname or IP address
+PORT = 65432        # The port used by the server
 
-    client_socket = socket.socket()  
-    client_socket.connect((host, port))  # connect to server
-    print('Enter the message or type Bye for closing the socket')
-    message = input(" -> ")  # take input
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((HOST, PORT))
+    s.sendall(b'Hello, world')
+    data = s.recv(1024)
 
-    while message.lower().strip() != 'Exit':
-        client_socket.send(message.encode())  # send message
-        data = client_socket.recv(1024).decode()  # receive response
-
-        print('Received from server: ' + data)  # show in terminal
-        print('Enter a message or type Exit for closing the socket')
-
-        message = input(" >>>> ")  # again take input
-
-    client_socket.close()  # close the connection
-
-
-if __name__ == '__main__':
-    client_program()
+print('Received', repr(data))
