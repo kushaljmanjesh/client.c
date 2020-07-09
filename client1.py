ea213a103b5a49c37ee1132eea7f5b095c183a2a
kushaljmanjesh <67941743+kushaljmanjesh@users.noreply.github.com>
2020-07-09 19:19:40 +0530

    client code

--- client1.py
+++ client1.py
@@ -1,12 +1,19 @@
-
 import socket
+def client_program():
+ host = socket.gethostname()  # The server's hostname or IP address
+ port = 55555        # The port used by the server
+ client_socket=socket.socket()
+ client_socket.connect((host, port))
+ 
+    
+ client_socket.send(b'Hello, world')
+ data = client_socket.recv(1024)
 
-HOST = '127.0.0.1'  # The server's hostname or IP address
-PORT = 65432        # The port used by the server
+ print('Received', repr(data))
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
-    s.sendall(b'Hello, world')
-    data = s.recv(1024)
+ client_socket.close()
 
-print('Received', repr(data))
+
+if __name__ == '__main__':
+    client_program()
+
