d78d64ae4b06e33355b0c9645e704aa51bc84216
kushaljmanjesh <67941743+kushaljmanjesh@users.noreply.github.com>
2020-07-27 12:44:38 +0530

    Soft remote with time control

--- Soft_Remote_GUI.py
+++ Soft_Remote_GUI.py
@@ -9,26 +9,41 @@ expression = ""
 # Function to update expressiom
 
 client_socket=socket.socket()
+at=time.time()
+print(at)#time start
 
+# Function to clear the contents
+# of text entry box
+def clear():
+   global expression
+   expression = ""
+   equation.set("")
 
 # in the text entry box
 def press(num):
+  bt=time.time()
+  
+  if((bt-at)%2!=0):
+  
     # point out the global expression variable
-    global expression
+   global expression
 
     # concatenation of string
-    expression = expression + str(num)
-
-    # update the expression by using set method
-    equation.set(expression)
-
-    client_socket.send(expression.encode())
-    # Look for the response
-    data = client_socket.recv(16).decode()
-    print ('received:' + data)
-  #  print('Enter the message or type Bye for closing the socket')
-    message = input(" -> ")   # take input
+   expression = expression + str(num)
     
+    # update the expression by using set method
+   equation.set(expression)
+
+   client_socket.send(expression.encode())
+    # Look for the response
+   data = client_socket.recv(16).decode()
+   print ('received:' + data)
+  #  print('Enter the message or type Bye for closing the socket')
+   message = input(" -> ")   # take input
+   
+  if((bt-at)%2==0) :
+   expression=""
+   equation.set("")
 def timedelay():
   
  key=keycode.get()
@@ -41,14 +56,14 @@ def timedelay():
  second=int(second)
  #frequency=input("enter the gap between presses in seconds")
  f=int(frequency)
- starttime=time.time()
+ 
  beginning=0
  
  while(beginning<=second):
    press(k)
    time.sleep(f) 
    beginning+=f
- finishtime=time.time()
+ 
  print("total time taken for the whole operation is",finishtime-starttime)
     
 def connect():
@@ -62,12 +77,7 @@ def connect():
 def terminate():
   print("connection terminated")
   client_socket.close()
-# Function to clear the contents
-# of text entry box
-def clear():
-    global expression
-    expression = ""
-    equation.set("")
+
   
 
 # Driver code 
@@ -141,9 +151,7 @@ if __name__ == "__main__":
   
 
 
- 
-    
-    
+   
  button1 = Button(gui, text='1', fg='black', bg='grey', 
                      command=lambda: press(2), height=1, width=10) 
  button1.grid(row=1, column=0) 
@@ -183,6 +191,8 @@ if __name__ == "__main__":
  button0 = Button(gui, text='0', fg='black', bg='grey', 
                      command=lambda: press(11), height=1, width=10) 
  button0.grid(row=4, column=1) 
+
+
   
  volup = Button(gui, text='VOLUP', fg='black', bg='grey', 
                   command=lambda: press("13"), height=2, width=7) 
@@ -257,44 +267,12 @@ if __name__ == "__main__":
                     command=lambda: press('49'), height=1,width=5) 
  fav.grid(row=5, column=1)
 
+ clear = Button(gui, text='Clear', fg='black', bg='red',
+                   command=clear, height=1, width=7)
+ clear.grid(row=18, column='2')
+
 
 
  # start the GUI
  gui.mainloop()
-
-
-   
-
-    
-
-'''
-#def client_program():
- #   host = socket.gethostname()  # as both code is running on same pc
-  #  port = 27015  # socket server port number
-
-   # client_socket = socket.socket()  # instantiate
-    #client_socket.connect((host, port))  # connect to the server
-    # Send data
-    client_socket.send(expression.encode())
-    # Look for the response
-    data = client_socket.recv(16).decode()
-    print ('received:' + data)
-    print('Enter the message or type Bye for closing the socket')
-    message = input(" -> ")   # take input
-
-    while message.lower().strip() != 'bye':
-        client_socket.send(message.encode())  # send message
-        data1 = client_socket.recv(1024).decode()  # receive response
-
-        print('Received from server: ' + data1)  # show in terminal
-        print('Enter the message or type Bye for closing the socket')
-        message = input(" -> ")  # again take input
-
-    client_socket.close()  # close the connection
-
-
-if __name__ == '__main__':
-    client_program()
-
-    '''
     
