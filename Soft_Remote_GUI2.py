bcac01a45ed1486c52a169dc844d7503b69fb068
kushaljmanjesh <67941743+kushaljmanjesh@users.noreply.github.com>
2020-07-27 13:19:35 +0530

    TO BE CHECKED FOR CORRECTION

--- Soft_Remote_GUI2.py
+++ Soft_Remote_GUI2.py
@@ -281,34 +281,3 @@ if __name__ == "__main__":
 
     
 
-'''
-#def client_program():
- #   host = socket.gethostname()  # as both code is running on same pc
-  #  port = 27015  # socket server port number
-
-   # client_socket = socket.socket()  # instantiate
-    #client_socket.connect((host, port))  # connect to the server
-    # Send data
-    client_socket.send(expression.encode())
-    # Look for the response
-    data = client_socket.recv(16).decode()
-    print ('received:' + data)
-    print('Enter the message or type Bye for closing the socket')
-    message = input(" -> ")   # take input
-
-    while message.lower().strip() != 'bye':
-        client_socket.send(message.encode())  # send message
-        data1 = client_socket.recv(1024).decode()  # receive response
-
-        print('Received from server: ' + data1)  # show in terminal
-        print('Enter the message or type Bye for closing the socket')
-        message = input(" -> ")  # again take input
-
-    client_socket.close()  # close the connection
-
-
-if __name__ == '__main__':
-    client_program()
-
-    '''
-    
